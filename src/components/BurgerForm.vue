3400c0a750e5b61ec6bcee533d596b0910ff5ae8
chagasleandro <chagasleandro>
2023-02-23 22:09:48 -0300

    add BurgerForm.vue

--- src/components/BurgerForm.vue
+++ src/components/BurgerForm.vue
@@ -20,8 +20,16 @@
                     <option value="maminha">Maminha</option>
                 </select>
             </div>
-            <div class="input-container">
-                <label for="opcionais">Selecione os opcionais:</label>
+            <div id="opcionais-container" class="input-container">
+                <label id="opcionais-title" for="opcionais">Selecione os opcionais:</label>
+                <div class="checkbox-container"> 
+                    <input type="checkbox" name="opcionais" v-model="opcionais" value="salame">
+                    <span>Salame</span>
+                </div>
+                <div class="checkbox-container"> 
+                    <input type="checkbox" name="opcionais" v-model="opcionais" value="salame">
+                    <span>Salame</span>
+                </div>
                 <div class="checkbox-container"> 
                     <input type="checkbox" name="opcionais" v-model="opcionais" value="salame">
                     <span>Salame</span>
@@ -41,5 +49,69 @@ export default {
 </script>
 
 <style scoped>
+    #burger-form {
+        max-width: 400px;
+        margin: 0 auto;
+    }
 
+    .input-container {
+        display: flex;
+        flex-direction: column;
+        margin-bottom: 20px;
+    }
+
+    label {
+        font-weight: bold;
+        margin-bottom: 15px;
+        color: #222;
+        padding: 5px 10px;
+        border-left: 4px solid #FCBA03;
+    }
+
+    input, select {
+        padding: 5px 10px;
+        width: 300px;
+    }
+
+    #opcionais-container {
+        flex-direction: row;
+        flex-wrap: wrap;
+    }
+
+    #opcionais-title {
+        width: 100%;
+    }
+
+    .checkbox-container {
+        display: flex;
+        align-items: flex-start;
+        width: 50%;
+        margin-bottom: 20px;
+    }
+
+    .checkbox-container span, 
+    .checkbos-container input {
+        width: auto;
+    }
+
+    .ckheckbox-xontainer span {
+        margin-left: 6px;
+        font-weight: bold;
+    }
+
+    .submit-btn {
+        background-color: #222;
+        color: #FCBA03;
+        font-weight: bold;
+        border: 2px solid #222;
+        padding: 10px;
+        margin: 0 auto;
+        cursor: pointer;
+        transition: .5s;
+    }
+
+    .submit-btn:hover {
+        background-color: transparent;
+        color: #222;
+    }
 </style>
